c4adb3897486765b645fcf1fd3f7316ef3d6316e
Allen Su <allenysu@gmail.com>
2020-12-21 17:29:03 +0800

    Initial Commit

---
+++ Project Cafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project Cafe
+//
+//  Created by Allen Su on 2020/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
